b9db714df40bd40d26b7009ec35d898e1ef8b65c
gribdesbois <gribdesbois@gmail.com>
2022-02-03 14:51:04 +0100

    might have gone overkill with donfig files . need to check if babel would be enough

--- vite-project/src/__tests__/App.test.jsx
+++ vite-project/src/__tests__/App.test.jsx
@@ -1,3 +1,19 @@
+import react from 'react'
+import {renderer} from 'react-test-renderer'
+
+//to test
+import App from '../App'
+
+
+//tests
 test('Renders main page correctly', () => {
+  //setup
+  renderer(<App/>)
+
+  //Pre expectations
+
+  //Init
+
+  //Post expectations
   expect(true).toBeTruthy()
 })
