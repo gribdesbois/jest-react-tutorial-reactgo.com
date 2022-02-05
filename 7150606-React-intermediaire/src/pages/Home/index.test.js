22ded51b367d5ff4e9ff3db5cb8fc885989386e0
gribdesbois <gribdesbois@gmail.com>
2022-02-05 13:03:02 +0100

    checking strings

--- 7150606-React-intermediaire/src/pages/Home/index.test.js
+++ 7150606-React-intermediaire/src/pages/Home/index.test.js
@@ -14,5 +14,13 @@ describe('Home component', () => {
         </ThemeProvider>
       </MemoryRouter>
     )
+    // screen.debug()
+    expect(
+      screen.getByRole('heading', {
+        level: 2,
+        name: 'Repérez vos besoins, on s’occupe du reste, avec les meilleurs talents',
+      })
+    ).toBeTruthy()
+    
   })
 })
